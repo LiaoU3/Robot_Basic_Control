c16a4df30091a24e1982fbb4da45d856f76b59f0
ahanjaya <hanjaya_mandala@yahoo.com>
2019-09-04 23:21:47 +0800

    Initial Commit

---
+++ Player/Config/calibration.lua
@@ -0,0 +1,33 @@
+module(..., package.seeall);
+
+---------------------------------------------
+-- Automatically generated calibration data
+---------------------------------------------
+cal={}
+
+--Initial values for each robots
+
+cal["barelangfc-ros"]={
+  servoBias={0,0,0,0,0,0, 0,0,0,0,0,0},
+  footXComp = 0,
+  footYComp = 0,
+  kickXComp = 0,
+  headPitchBiasComp = 0*math.pi/180;
+  armBias={0,0,0,0,0,0},
+  pid = 1, --NEW FIRMWARE
+};
+
+------------------------------------------------------------
+--Auto-appended calibration settings
+------------------------------------------------------------
+--Cleaned up on 06/28/2012 @ GRASP
+
+-- Updated date: Tue Feb 19 15:17:40 2019
+cal["barelangfc-ros"].servoBias={0,0,0,0,0,0,0,0,0,0,0,0,};
+cal["barelangfc-ros"].footXComp=-0.009;
+cal["barelangfc-ros"].kickXComp=0.000;
+
+-- Updated date: Sat Mar  2 17:57:51 2019
+cal["barelangfc-ros"].servoBias={0,0,0,0,0,0,0,0,0,0,0,0,};
+cal["barelangfc-ros"].footXComp=-0.009;
+cal["barelangfc-ros"].kickXComp=0.000;
